63d5ab0f46e9553bcd80e9d380f135a872f2f376
Ice <panittasak@gmail.com>
2026-02-06 18:06:47 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+routes.push({
+  path: 'summary',
+  loadComponent: () =>
+    import('../app/feature/summary/summary.component').then(
+      (m) => m.SummaryComponent,
+    ),
+});
--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../side-bar/side-bar.component';
+import { Router, RouterOutlet } from '@angular/router';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, SideBarComponent],
+  imports: [CommonModule, SideBarComponent, RouterOutlet, routes],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
